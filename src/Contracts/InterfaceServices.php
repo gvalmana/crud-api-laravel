9984e0da8cf3dcdf868cdd15bfdd26ef5018ed03
Lead Backend <leadback@vixidev.com>
2023-11-13 14:17:38 -0500

    eliminados los métodos del destroy y delete

--- src/Contracts/InterfaceServices.php
+++ src/Contracts/InterfaceServices.php
@@ -11,8 +11,4 @@ interface InterfaceServices
     public function saveArray(array $attributes, $scenario = 'create');
     public function update(string|int  $id, array $attributes);
     public function updateMultiple(array $attributes);
-    public function destroy(string|int $id);
-    public function destroyByIds(array $ids);
-    public function restore(string|int $id);
-    public function restoreByIds(array $ids);
 }
