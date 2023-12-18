5b933b263c72be826aeef98267219875504caa45
gvalmana <gustavo.shotokai@gmail.com>
2023-12-18 11:20:37 -0500

    refactorizado el createupdate repository

--- src/Repositories/RepositoryCreateOrUpdate.php
+++ src/Repositories/RepositoryCreateOrUpdate.php
@@ -31,42 +31,36 @@ abstract class CreateOrUpdateRepository implements InterfaceUpdateOrCreateReposi
 
     public function selfValidate(array $attributes, $scenario = 'create', $specific = false)
     {
-        $errors = [];
-        if (isset($attributes[$this->modelClass->getPrimaryKey()]) && $scenario == 'create') {
-            $scenario = "update";
+        if (isset($attributes[$this->modelClass->getPrimaryKey()]) && $scenario === 'create') {
+            $scenario = 'update';
         }
+
         $this->modelClass->setScenario($scenario);
         $this->modelClass->fill($attributes);
-        $validation = $this->modelClass->selfValidate($this->modelClass->getScenario(), $specific, false);
-        return $validation;
+
+        return $this->modelClass->selfValidate($this->modelClass->getScenario(), $specific, false);
     }
 
     public function create(array $params)
     {
-        if (isset($params['data']) && is_array($params['data'])) {
-            $result = $this->saveArray($params['data']);
-        } else {
-            $result = $this->save($params);
-        }
+        $result = isset($params['data']) && is_array($params['data'])
+            ? $this->saveArray($params['data'])
+            : $this->save($params);
 
         return $result;
     }
 
     public function save(array $attributes, $scenario = 'create')
     {
-        $success = true;
-        $model = null;
-        $errors = null;
-        $this->initializeModel($attributes, $scenario);
-        $validateResult = $this->selfValidate($attributes, $this->modelClass->getScenario());
-        if (!$validateResult['success']) {
-            $success = false;
-            $errors = $validateResult['errors'];
-            $model = $validateResult['model'];
-        } else {
-            $this->modelClass->fill($attributes);
-            $this->modelClass->save();
-            $model = $this->modelClass->getAttributes();
+        $model = $this->initializeModel($attributes, $scenario);
+
+        $validateResult = $this->selfValidate($attributes, $model->getScenario());
+        $success = $validateResult['success'];
+        $errors = $validateResult['errors'];
+
+        if ($success) {
+            $model->fill($attributes);
+            $model->save();
         }
 
         return compact('success', 'errors', 'model');
@@ -74,61 +68,65 @@ abstract class CreateOrUpdateRepository implements InterfaceUpdateOrCreateReposi
 
     public function saveArray(array $attributes, $scenario = 'create')
     {
-        $success = true;
         $models = [];
         $errors = [];
-        foreach ($attributes as $key=>$data) {
+
+        foreach ($attributes as $key => $data) {
             $result = $this->save($data, $scenario);
+
             if (!$result['success']) {
-                $success = false;
                 $errors[$key][] = $result['errors'];
             }
-            $models['models'][] = $result['model'];
+
+            $models[] = $result['model'];
         }
+
+        $success = empty($errors);
 
         return compact('success', 'models', 'errors');
     }
 
     public function update($id, array $attributes)
     {
-        $success = true;
-        $model = null;
-        $errors = null;
-        $this->modelClass = $this->modelClass::findOrFail($id);
-        $this->modelClass->setScenario(RestModel::UPDATE_SCENARIO);
-        $specific = isset($attributes["_specific"]) ? $attributes["_specific"] : false;
-        $validation = $this->selfValidate($attributes, $this->modelClass->getScenario(), $specific);
+        $model = $this->modelClass::findOrFail($id);
+        $model->setScenario(RestModel::UPDATE_SCENARIO);
+
+        $specific = $attributes["_specific"] ?? false;
+        $validation = $this->selfValidate($attributes, $model->getScenario(), $specific);
+
         if (!$validation['success']) {
-            $success = $validation['success'];
-            $errors = $validation['errors'];
-            $model = $validation['model'];
-            return compact('success', 'errors', 'model');
-        } else {
-            $this->modelClass->save();
-            $model = $this->modelClass;
+            return $validation;
         }
-        return compact('success', 'errors','model');
+
+        $model->save();
+
+        return [
+            'success' => true,
+            'errors' => null,
+            'model' => $model,
+        ];
     }
 
     public function updateMultiple(array $params)
     {
         $success = true;
         $models = [];
+
         foreach ($params as $index => $item) {
             $id = $item[$this->modelClass->getPrimaryKey()];
             $res = $this->update($item, $id);
-            if (!$res['success']) {
-                $success = false;
-            }
-            $models['models'][$index] = $res;
+            $success = $success && $res['success'];
+            $models[] = $res;
         }
+
         return compact('success', 'models');
     }
 
     private function initializeModel(array $attributes, $scenario)
     {
-        if (isset($attributes[$this->modelClass->getPrimaryKey()])) {
-            $this->modelClass = $this->modelClass::find($attributes[$this->modelClass->getPrimaryKey()]);
+        $primaryKey = $this->modelClass->getPrimaryKey();
+        if (isset($attributes[$primaryKey])) {
+            $this->modelClass = $this->modelClass::find($attributes[$primaryKey]);
             $this->modelClass->setScenario(RestModel::UPDATE_SCENARIO);
         } else {
             $this->modelClass = new $this->modelClass;
